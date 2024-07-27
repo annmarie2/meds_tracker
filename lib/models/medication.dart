b1d92a855759d326cde536519efb99925c02d744
James <jammiesprideful@gmail.com>
2024-07-26 22:07:16 -0600

    Add persistence file

--- lib/models/medication.dart
+++ lib/models/medication.dart
@@ -1,8 +1,45 @@
-class Medication {
-    final String name;
-    final DateTime lastTriggered;
-    final Duration interval;
-    final bool doAlarm;
+import 'dart:convert';
 
-    Medication({required this.name, required this.lastTriggered, required this.interval, this.doAlarm = false});
+class Medication {
+  final String name;
+  final DateTime lastTriggered;
+  final Duration interval;
+  final bool doAlarm;
+
+  Medication({
+    required this.name,
+    required this.lastTriggered,
+    required this.interval,
+    this.doAlarm = false
+  });
+
+  // Convert a Medication instance to a JSON object
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'lastTriggered': lastTriggered.toIso8601String(),
+      'interval': interval.inMilliseconds, // Store duration as milliseconds
+      'doAlarm': doAlarm,
+    };
+  }
+
+  // Create a Medication instance from a JSON object
+  factory Medication.fromJson(Map<String, dynamic> json) {
+    return Medication(
+      name: json['name'],
+      lastTriggered: DateTime.parse(json['lastTriggered']),
+      interval: Duration(milliseconds: json['interval']),
+      doAlarm: json['doAlarm'] ?? false,
+    );
+  }
+
+  // Optionally, convert a Medication instance to a JSON string
+  String toJsonString() {
+    return json.encode(toJson());
+  }
+
+  // Optionally, create a Medication instance from a JSON string
+  factory Medication.fromJsonString(String jsonString) {
+    return Medication.fromJson(json.decode(jsonString));
+  }
 }
