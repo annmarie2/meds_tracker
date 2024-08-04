362f6ac5c03f9fb67f620a317d4f3f8c3c0664c9
Ann Marie Humble <A02324311@aggies.usu.edu>
2024-08-04 09:42:24 -0600

    prettified edit page

--- lib/edit_view.dart
+++ lib/edit_view.dart
@@ -78,88 +78,114 @@ class _EditViewState extends State<EditView> {
     // var appState = context.watch<MainAppState>();
     return Scaffold(
       appBar: AppBar(
-        title: Text('Edit Medication'),
+        title: Text(
+          'Edit Medication',
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        iconTheme: IconThemeData(
+          color: Theme.of(context).colorScheme.onPrimary,
+        ),
       ),
       body: Column(
         children: [
+          SizedBox(height: 20),
           IntrinsicWidth(
             child: TextFormField(
               controller: _nameController,
               decoration: InputDecoration(
-                border: OutlineInputBorder(),
+                border: UnderlineInputBorder(),
               ),
               style: TextStyle(
-                fontSize: 24.0,
+                fontSize: Theme.of(context).textTheme.titleLarge?.fontSize,
+                color: Theme.of(context).colorScheme.primary,
               ),
               onChanged: (value) {},
             ),
           ),
           SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Period: "),
-              SizedBox(width: 10), // Add some space between the text and the input field
-              IntrinsicWidth(
-                child: TextFormField(
-                  controller: _durationController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                  onChanged: (value) {},
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text("Period: "),
+                    SizedBox(width: 10),
+                    IntrinsicWidth(
+                      child: TextFormField(
+                        controller: _durationController,
+                        decoration: InputDecoration(
+                          border: UnderlineInputBorder(),
+                        ),
+                        style: TextStyle(
+                          fontSize: Theme.of(context).textTheme.bodyLarge?.fontSize,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        onChanged: (value) {},
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text('hours'),
+                  ],
                 ),
-              ),
-              SizedBox(width: 10),
-              Text('hours'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Last time taken: '),
-              SizedBox(width: 10),
-              IntrinsicWidth(
-                child: TextFormField(
-                  controller: _lastTriggeredController,
-                  // initialValue: DateFormat('MMMM d, yyyy - h:mm a').format(widget.med.lastTriggered), // Display the time in a human-readable format                  
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                  onChanged: (value) {
-                    // TODO: Handle changes to the input field if necessary
-                  },
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text('Last taken: '),
+                    SizedBox(width: 10),
+                    IntrinsicWidth(
+                      child: TextFormField(
+                        controller: _lastTriggeredController,
+                        decoration: InputDecoration(
+                          border: UnderlineInputBorder(),
+                        ),
+                        style: TextStyle(
+                          fontSize: Theme.of(context).textTheme.bodyLarge?.fontSize,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                        onChanged: (value) {},
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Text('Alarm: '),
+                    SizedBox(width: 10),
+                    Checkbox(
+                      value: _alarm,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          _alarm = value ?? false;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Alarm: '),
-              SizedBox(width: 10),
-              Checkbox(
-                value: _alarm,
-                onChanged: (bool? value) {
-                  // TODO: Handle changes to the checkbox if necessary
-                  setState(() {
-                    _alarm = value ?? false;
-                  });
-                },
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: _save,
-                child: Text('Save'),
-              ),
-              ElevatedButton(
-                onPressed: _delete,
-                child: Text('Delete'),
-              ),
-            ],
+          Center(
+            child: Row(
+              mainAxisSize: MainAxisSize.min, // Ensures the Row takes up only as much space as its children
+              children: [
+                ElevatedButton(
+                  onPressed: _save,
+                  child: Text('Save'),
+                ),
+                SizedBox(width: 40),
+                ElevatedButton(
+                  onPressed: _delete,
+                  child: Text('Delete'),
+                ),
+              ],
+            ),
           ),
         ],
       ),
